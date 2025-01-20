b503c8bf0701f0f0c148a04df0724fff8e77a596
Muhsin Ahamed Fazal <muhsin.fazal@bigbinary.com>
2025-01-20 14:08:19 +0530

    Initial setup

---
+++ test/string_calculator_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../string_calculator'
+
+class StringCalculatorTest < Minitest::Test
+  def setup
+    @calculator = StringCalculator.new
+  end
+end
