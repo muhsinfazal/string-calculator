4f0369c924d243db28549b858b020b492c084699
Muhsin Ahamed Fazal <muhsin.fazal@bigbinary.com>
2025-01-20 15:45:03 +0530

    Handled empty string sum case

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class StringCalculator
-  def add; end
+  def add(string_numbers)
+    0 if string_numbers.empty?
+  end
 end
--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -7,4 +7,10 @@ class StringCalculatorTest < Minitest::Test
   def setup
     @calculator = StringCalculator.new
   end
+
+  def test_empty_string_should_return_sum_zero
+    sum = @calculator.add('')
+
+    assert_equal 0, sum
+  end
 end
